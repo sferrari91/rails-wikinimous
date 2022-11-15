2ced5c8071ad64d78ea9c9c08b494fde3af4cded
Santiago Ferrari <113905797+sferrari91@users.noreply.github.com>
2022-11-15 16:03:01 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do
+  articles = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Cannabis.buzzword
+  )
+  articles.save!
+end
